2b3ef5f52a7889495d4d3ee5a9bf06962e37a8ff
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2022-04-12 17:53:45 +0300

    adjust array subtypes when re json schema

--- reverse_engineering/api.js
+++ reverse_engineering/api.js
@@ -16,7 +16,8 @@ const HiveParser = require('./parser/Hive/HiveParser.js');
 const hqlToCollectionsVisitor = require('./hqlToCollectionsVisitor.js');
 const commandsService = require('./commandsService');
 const ExprErrorListener = require('./antlrErrorListener');
-const { getCleanedUrl } = require('../forward_engineering/helpers/generalHelper')
+const { getCleanedUrl } = require('../forward_engineering/helpers/generalHelper');
+const mapJsonSchema = require('./thriftService/mapJsonSchema');
 
 module.exports = {
 
@@ -257,6 +258,36 @@ module.exports = {
 			callback(handledError);
 		}
 	},
+
+	adaptJsonSchema(data, logger, callback, app) {
+		try {
+			setDependencies(app);
+			const _ = app.require('lodash');
+			const jsonSchema = JSON.parse(data.jsonSchema);
+			const result = mapJsonSchema(_)(jsonSchema, {}, (schema, parentJsonSchema, key) => {
+				if (schema.type === 'array' && !schema.subtype) {
+					return {
+						...schema,
+						subtype: getArraySubtypeByChildren(_, schema),
+					};
+				} else {
+					return schema;
+				}
+			});
+		
+			callback(null, {
+				...data,
+				jsonSchema: JSON.stringify(result)
+			});
+		} catch (error) {
+			const err = {
+				message: error.message,
+				stack: error.stack,
+			};
+			logger.log('error', err, 'Remove nulls from JSON Schema');
+			callback(err);
+		}
+	},
 };
 
 const handleFileData = filePath => {
@@ -299,4 +330,54 @@ const createViewPackage = (name, viewData = {}) => {
 			type: 'postgres'
 		}
 	};
+};
+
+const getArraySubtypeByChildren = (_, arraySchema) => {
+	const subtype = (type) => `array<${type}>`;
+
+	if (!arraySchema.items) {
+		return;
+	}
+
+	if (Array.isArray(arraySchema.items) && _.uniq(arraySchema.items.map(item => item.type)).length > 1) {
+		return subtype("union");
+	}
+
+	let item = Array.isArray(arraySchema.items) ? arraySchema.items[0] : arraySchema.items;
+
+	if (!item) {
+		return;
+	}
+
+	switch(item.type) {
+		case 'string':
+		case 'text':
+			return subtype("txt");
+		case 'number':
+		case 'numeric':
+			return subtype("num");
+		case 'interval':
+			return subtype("intrvl");
+		case 'object':
+		case 'struct':
+			return subtype("struct");
+		case 'array':
+			return subtype("array");
+		case 'map':
+			return subtype("map");
+		case "union":
+			return subtype("union");
+		case "timestamp":
+			return subtype("ts");
+		case "date":
+			return subtype("date");
+	}
+
+	if (item.items) {
+		return subtype("array");
+	}
+
+	if (item.properties) {
+		return subtype("struct");
+	}
 };
