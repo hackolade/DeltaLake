f7d115a659202ae2b64620c7a7cc41bfd7351974
Dmytro <beardimon2001@gmail.com>
2022-09-28 11:32:23 +0300

    fix RE from instance for tableProperties

--- reverse_engineering/sqlBaseToCollectionsVisitor.js
+++ reverse_engineering/sqlBaseToCollectionsVisitor.js
@@ -158,7 +158,7 @@ class Visitor extends SqlBaseVisitor {
 			createFileFormat: this.visitIfExists(ctx, 'createFileFormat', [])[0],
 			locationSpec: this.visitIfExists(ctx, 'locationSpec', [])[0],
 			commentSpec: this.visitIfExists(ctx, 'commentSpec', [])[0],
-			tableProperties: this.visitIfExists(ctx, 'tableProperties', {start:0, stop:-1})?.[0]?.[1],
+			tableProperties: this.visitIfExists(ctx, 'tableProperties', [])?.[0]?.[1],
 			tableOptions: this.visitIfExists(ctx, 'tableOptions', {start:0, stop:-1})?.[0]?.[1],
 		}
 	}
@@ -168,10 +168,9 @@ class Visitor extends SqlBaseVisitor {
 	}
 
 	visitTableProperty(ctx) {
-		return {
-			propertyKey: getLabelValue(ctx, 'key'),
-			propertyValue: getLabelValue(ctx, 'value'),
-		}
+		const propertyKey = getName(ctx.key);
+		const propertyValue = getName(ctx.value);
+		return ctx.value === undefined ? { propertyKey } : { propertyKey, propertyValue };
 	}
 
 	visitLocationSpec(ctx) {
