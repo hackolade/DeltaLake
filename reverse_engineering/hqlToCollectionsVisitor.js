71cad3587972530d717401a98e42d069a5bfacbc
Miedviediev Dmytro <beardimon2001@gmail.com>
2022-12-27 15:05:42 +0200

    Add numBuckets cast to number

--- reverse_engineering/hqlToCollectionsVisitor.js
+++ reverse_engineering/hqlToCollectionsVisitor.js
@@ -520,7 +520,7 @@ class Visitor extends HiveParserVisitor {
         return {
             type: UPDATE_ENTITY_LEVEL_DATA_COMMAND,
             data: {
-                numBuckets: ctx.Number().getText(),
+                numBuckets: Number(ctx.Number().getText()),
             },
         };
     }
