ca1019bc7b3336f0816f285e7cdd5e43083e29f2
unknown <unknown>
2023-05-19 15:16:07 +0300

    Removed obsolete check for dropped comments on schemas and entities

--- forward_engineering/enums/dropStatements.js
+++ forward_engineering/enums/dropStatements.js
@@ -7,8 +7,6 @@ const DropStatements = Object.freeze({
 	DROP_COLUMNS: 'DROP COLUMNS',
 	// This statement is used when a column's comment is dropped
 	COMMENT: "COMMENT '';",
-	// This statement is used when table/schema 's comment is dropped
-	IS_NULL: 'IS NULL;',
 	// This statement is used when not-null constraint is dropped
 	DROP_NOT_NULL: 'DROP NOT NULL;',
 	// This statement is used when check constraint is dropped
