7f4dff871fa01ab192386d6b15dc8352e9bdd770
unknown <unknown>
2023-11-29 12:40:21 +0200

    Added ability to map MAP<> type to DML if it has "simple" data types

--- forward_engineering/sampleGeneration/mapInsertSampleToDml.js
+++ forward_engineering/sampleGeneration/mapInsertSampleToDml.js
@@ -22,6 +22,57 @@ const mapInsertNumberSampleToDml = (column, sample) => {
     return Number(sample);
 }
 
+/**
+ * @param column {Object}
+ * @param sample {any}
+ * */
+const mapSqlCodeToDml = (column, sample) => {
+    return (sample || '').toString();
+}
+
+/**
+ * @param column {Object}
+ * @param sample {any}
+ * */
+const mapJsonObjectToDml = (column, sample) => {
+    if (column.type !== 'jsonObject') {
+        return '';
+    }
+    return JSON.stringify(sample);
+}
+
+const mapMapToDml = (column, sample) => {
+    const keyType = column.keyType || 'text';
+    /**
+     * @type {InsertSampleMapper}
+     * */
+    const keyMapper = typeToMapperMap.get(keyType) || (() => {});
+
+    const mapKeyValuePairs = [];
+    const properties = column.properties || {};
+    for (const propertyName of Object.keys(properties)) {
+        const propertyJsonSchema = properties[propertyName] || {};
+        const propertyType = propertyJsonSchema.type || 'text';
+        /**
+         * @type {InsertSampleMapper}
+         * */
+        const valueMapper = typeToMapperMap.get(propertyType) || (() => {});
+
+        if (mapKeyValuePairs.length !== 0) {
+            mapKeyValuePairs.push(', ');
+        }
+
+        const dmlKey = keyMapper(column, propertyName);
+        const dmlValue = valueMapper(propertyJsonSchema, sample[propertyName]);
+        mapKeyValuePairs.push(dmlKey);
+        mapKeyValuePairs.push(', ');
+        mapKeyValuePairs.push(dmlValue);
+    }
+
+    mapKeyValuePairs.unshift('MAP(');
+    mapKeyValuePairs.push(')');
+    return mapKeyValuePairs.join('');
+}
 
 /**
  * @type {Map<string, InsertSampleMapper>}
@@ -29,6 +80,10 @@ const mapInsertNumberSampleToDml = (column, sample) => {
 const typeToMapperMap = new Map()
     .set('text', mapInsertStringSampleToDml)
     .set('numeric', mapInsertNumberSampleToDml)
+    .set('timestamp', mapSqlCodeToDml)
+    .set('date', mapSqlCodeToDml)
+    .set('interval', mapSqlCodeToDml)
+    .set('map', mapMapToDml)
 
 /**
  * @param column {Object}
