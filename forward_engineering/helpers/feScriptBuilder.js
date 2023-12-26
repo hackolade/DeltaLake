f1000984f8f5f869f97b9b76322a1e3b0c9c3f3c
unknown <unknown>
2023-12-26 11:29:52 +0200

    - Fixed types in jsdocs in feScriptBuilder.js - Fixed formatting in feScriptBuilder.js

--- forward_engineering/helpers/feScriptBuilder.js
+++ forward_engineering/helpers/feScriptBuilder.js
@@ -35,7 +35,7 @@ const {getIndexes} = require("./indexHelper");
 const {buildScript, getName, getTab, isSupportUnityCatalog, isSupportNotNullConstraints} = require("../utils/general");
 const {getViewScript} = require("./viewHelper");
 const {generateSamplesScript, getDataForSampleGeneration} = require('../sampleGeneration/sampleGenerationService');
-const { batchProcessFile } = require('../../reverse_engineering/helpers/fileHelper');
+const {batchProcessFile} = require('../../reverse_engineering/helpers/fileHelper');
 
 /**
  * @typedef {{
@@ -55,8 +55,6 @@ const { batchProcessFile } = require('../../reverse_engineering/helpers/fileHelp
  *     modelDefinitions: ModelDefinitions,
  *     internalDefinitions: InternalDefinitions,
  *     containerData: ContainerData,
- *     arePkFkConstraintsAvailable: boolean,
- *     areNotNullConstraintsAvailable: boolean,
  *     includeRelationshipsInEntityScripts: boolean,
  *     entitiesJsonSchema: EntitiesJsonSchema,
  *     includeSamplesInEntityScripts: boolean,
@@ -69,14 +67,14 @@ const { batchProcessFile } = require('../../reverse_engineering/helpers/fileHelp
  * @return {(dto: EntityLevelFEScriptData) => string}
  * */
 const buildEntityLevelFEScript = (data, app) => ({
-    externalDefinitions,
-    modelDefinitions,
-    jsonSchema,
-    internalDefinitions,
-    containerData,
-    entityData,
-    modelData,
-}) => {
+                                                     externalDefinitions,
+                                                     modelDefinitions,
+                                                     jsonSchema,
+                                                     internalDefinitions,
+                                                     containerData,
+                                                     entityData,
+                                                     modelData,
+                                                 }) => {
     const _ = app.require('lodash');
     const dbVersion = data.modelData[0].dbVersion;
     const arePkFkConstraintsAvailable = isSupportUnityCatalog(dbVersion);
@@ -139,42 +137,47 @@ const getContainerLevelViewScriptDtos = (data, _) => {
 /**
  * @param _ {any}
  * @return {({
- * data: CoreData,
- * includeSamplesInEntityScripts: boolean,
- * entitiesJsonSchema: Record<string, Object>,
- * entityId: string,
+ *      data: CoreData,
+ *      includeSamplesInEntityScripts: boolean,
+ *      entitiesJsonSchema: Record<string, Object>,
+ *      entityId: string,
  * }) => Promise<string>}
-*/
-const getSampleScriptForContainerLevelScript = (_) => async ({data, includeSamplesInEntityScripts, entitiesJsonSchema, entityId}) => {
-	const sampleScripts = [];
-	if (includeSamplesInEntityScripts) {
-		const { jsonData, entitiesData } = getDataForSampleGeneration(data, entitiesJsonSchema);
-		const entityJsonSchema = entitiesJsonSchema[entityId] || {};
-		if (jsonData) {
-			const demoSampleJsonData = jsonData[entityId] || {};
-			const demoSample = generateSamplesScript(_)(entityJsonSchema, [demoSampleJsonData]);
-			sampleScripts.push(demoSample);
-		}
-		if (entitiesData) {
-			const entityData = entitiesData[entityId];
-			const { filePath, jsonSchema, jsonData } = entityData;
+ */
+const getSampleScriptForContainerLevelScript = (_) => async ({
+                                                                 data,
+                                                                 includeSamplesInEntityScripts,
+                                                                 entitiesJsonSchema,
+                                                                 entityId
+                                                             }) => {
+    const sampleScripts = [];
+    if (includeSamplesInEntityScripts) {
+        const {jsonData, entitiesData} = getDataForSampleGeneration(data, entitiesJsonSchema);
+        const entityJsonSchema = entitiesJsonSchema[entityId] || {};
+        if (jsonData) {
+            const demoSampleJsonData = jsonData[entityId] || {};
+            const demoSample = generateSamplesScript(_)(entityJsonSchema, [demoSampleJsonData]);
+            sampleScripts.push(demoSample);
+        }
+        if (entitiesData) {
+            const entityData = entitiesData[entityId];
+            const {filePath, jsonSchema, jsonData} = entityData;
 
-			const demoSample = generateSamplesScript(_)(jsonSchema, [jsonData]);
-			sampleScripts.push(demoSample);
+            const demoSample = generateSamplesScript(_)(jsonSchema, [jsonData]);
+            sampleScripts.push(demoSample);
 
-			await batchProcessFile({
-				filePath,
-				batchSize: 1,
-				parseLine: line => JSON.parse(line),
-				batchHandler: async batch => {
-					const sample = generateSamplesScript(_)(jsonSchema, batch);
-					sampleScripts.push(sample);
-				},
-			});
-		}
-	}
+            await batchProcessFile({
+                filePath,
+                batchSize: 1,
+                parseLine: line => JSON.parse(line),
+                batchHandler: async batch => {
+                    const sample = generateSamplesScript(_)(jsonSchema, batch);
+                    sampleScripts.push(sample);
+                },
+            });
+        }
+    }
 
-	return sampleScripts.join('\n\n');
+    return sampleScripts.join('\n\n');
 }
 
 
@@ -184,64 +187,64 @@ const getSampleScriptForContainerLevelScript = (_) => async ({data, includeSampl
  * @return {(data: ContainerLevelFEScriptData) => Promise<Array<ContainerLevelEntityDto>>}
  * */
 const getContainerLevelEntitiesScriptDtos = (app, data) => async ({
-    externalDefinitions,
-    modelDefinitions,
-    internalDefinitions,
-    containerData,
-    entitiesJsonSchema,
-    arePkFkConstraintsAvailable,
-    areNotNullConstraintsAvailable,
-    includeRelationshipsInEntityScripts,
-    includeSamplesInEntityScripts,
-}) => {
-	const _ = app.require('lodash');
-	const scriptDtos = [];
+                                                                      externalDefinitions,
+                                                                      modelDefinitions,
+                                                                      internalDefinitions,
+                                                                      containerData,
+                                                                      entitiesJsonSchema,
+                                                                      arePkFkConstraintsAvailable,
+                                                                      areNotNullConstraintsAvailable,
+                                                                      includeRelationshipsInEntityScripts,
+                                                                      includeSamplesInEntityScripts,
+                                                                  }) => {
+    const _ = app.require('lodash');
+    const scriptDtos = [];
 
-	for (const entityId of data.entities) {
-		const entityData = data.entityData[entityId];
+    for (const entityId of data.entities) {
+        const entityData = data.entityData[entityId];
 
-		const likeTableData = data.entityData[getTab(0, entityData)?.like];
-		const entityJsonSchema = entitiesJsonSchema[entityId];
-		const definitions = [internalDefinitions[entityId], modelDefinitions, externalDefinitions];
-		const createTableStatementArgs = [containerData, entityData, entityJsonSchema, definitions];
+        const likeTableData = data.entityData[getTab(0, entityData)?.like];
+        const entityJsonSchema = entitiesJsonSchema[entityId];
+        const definitions = [internalDefinitions[entityId], modelDefinitions, externalDefinitions];
+        const createTableStatementArgs = [containerData, entityData, entityJsonSchema, definitions];
 
-		const tableStatement = getTableStatement(app)(
-			...createTableStatementArgs,
-			arePkFkConstraintsAvailable,
-			areNotNullConstraintsAvailable,
-			likeTableData,
-		);
+        const tableStatement = getTableStatement(app)(
+            ...createTableStatementArgs,
+            arePkFkConstraintsAvailable,
+            areNotNullConstraintsAvailable,
+            likeTableData,
+        );
 
-		const indexScript = getIndexes(_)(...createTableStatementArgs);
+        const indexScript = getIndexes(_)(...createTableStatementArgs);
 
-		let relationshipScripts = [];
-		if (includeRelationshipsInEntityScripts && arePkFkConstraintsAvailable) {
-			const relationshipsWithThisTableAsChild = data.relationships.filter(
-				relationship => relationship.childCollection === entityId,
-			);
-			relationshipScripts = getCreateRelationshipScripts(app)(relationshipsWithThisTableAsChild, entitiesJsonSchema);
-		}
+        let relationshipScripts = [];
+        if (includeRelationshipsInEntityScripts && arePkFkConstraintsAvailable) {
+            const relationshipsWithThisTableAsChild = data.relationships.filter(
+                relationship => relationship.childCollection === entityId,
+            );
+            relationshipScripts = getCreateRelationshipScripts(app)(relationshipsWithThisTableAsChild, entitiesJsonSchema);
+        }
 
-		const sampleScript = await getSampleScriptForContainerLevelScript(_)({
-			data,
-			entitiesJsonSchema,
-			entityId,
-			includeSamplesInEntityScripts,
-		});
+        const sampleScript = await getSampleScriptForContainerLevelScript(_)({
+            data,
+            entitiesJsonSchema,
+            entityId,
+            includeSamplesInEntityScripts,
+        });
 
-		let tableScript = buildScript([tableStatement, indexScript, ...relationshipScripts]);
-		if (sampleScript) {
-			// This is because SQL formatter breaks some "INSERT" statements with complex types
-			tableScript = [tableScript, sampleScript].join('\n');
-		}
+        let tableScript = buildScript([tableStatement, indexScript, ...relationshipScripts]);
+        if (sampleScript) {
+            // This is because SQL formatter breaks some "INSERT" statements with complex types
+            tableScript = [tableScript, sampleScript].join('\n');
+        }
 
-		scriptDtos.push({
-			name: getName(entityData[0]),
-			script: tableScript,
-		});
-	}
+        scriptDtos.push({
+            name: getName(entityData[0]),
+            script: tableScript,
+        });
+    }
 
-	return scriptDtos;
+    return scriptDtos;
 }
 
 /**
@@ -264,14 +267,14 @@ const getContainerLevelEntitiesScriptDtos = (app, data) => async ({
  * }>}
  * */
 const buildContainerLevelFEScriptDto = (data, app) => async ({
-    internalDefinitions,
-    externalDefinitions,
-    modelDefinitions,
-    entitiesJsonSchema,
-    containerData,
-    includeRelationshipsInEntityScripts,
-    includeSamplesInEntityScripts,
-}) => {
+                                                                 internalDefinitions,
+                                                                 externalDefinitions,
+                                                                 modelDefinitions,
+                                                                 entitiesJsonSchema,
+                                                                 containerData,
+                                                                 includeRelationshipsInEntityScripts,
+                                                                 includeSamplesInEntityScripts,
+                                                             }) => {
     const _ = app.require('lodash');
     const dbVersion = data.modelData[0].dbVersion;
     const arePkFkConstraintsAvailable = isSupportUnityCatalog(dbVersion);
