5b8b18772bd0608ee291355d12afb51bd2fc09fd
Serhii Filonenko <serhii.filonenko@binary-studio.com>
2025-01-15 13:54:27 +0200

    HCK-9462: update grammar to parse CLUSTER BY clause

--- reverse_engineering/parser/Hive/HiveParserVisitor.js
+++ reverse_engineering/parser/Hive/HiveParserVisitor.js
@@ -375,6 +375,11 @@ class HiveParserVisitor extends antlr4.tree.ParseTreeVisitor {
 		return this.visitChildren(ctx);
 	}
 
+	// Visit a parse tree produced by HiveParser#alterStatementSuffixClusterBy.
+	visitAlterStatementSuffixClusterBy(ctx) {
+		return this.visitChildren(ctx);
+	}
+
 	// Visit a parse tree produced by HiveParser#alterTblPartitionStatementSuffixSkewedLocation.
 	visitAlterTblPartitionStatementSuffixSkewedLocation(ctx) {
 		return this.visitChildren(ctx);
