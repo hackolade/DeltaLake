ff7dcaaaf864d376dfc0ad08a453c2fbd44ab35a
Vitalii Yarmus <71256742+Vitalii4as@users.noreply.github.com>
2025-05-06 14:02:55 +0300

    HCK-10922: expose RE from instance API in the browser (#262)

--- api/re.js
+++ api/re.js
@@ -1,4 +1,1 @@
-const { adaptJsonSchema } = require('../reverse_engineering/adaptJsonSchema');
-const { parseViewStatement } = require('../reverse_engineering/parseViewStatement');
-
-module.exports = { adaptJsonSchema, parseViewStatement };
+module.exports = require('../reverse_engineering/api');
