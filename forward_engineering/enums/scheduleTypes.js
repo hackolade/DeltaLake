d20dbf121d3536f6b9ffca71223050eb2861c532
ivan-m-dev <ivan.malenko@binary-studio.com>
2026-01-21 15:52:02 +0100

    HCK-14320: add CREATE and SCHEDULE clauses

---
+++ forward_engineering/enums/scheduleTypes.js
@@ -0,0 +1,11 @@
+const ScheduleTypesEnum = Object.freeze({
+	NONE: 'None',
+	CRON: 'Cron',
+	EVERY: 'Every',
+	TRIGGER_ON_UPDATE: 'Trigger on Update',
+	TRIGGER_ON_UPDATE_BETA: 'Trigger on Update (Beta)',
+});
+
+module.exports = {
+	ScheduleTypesEnum,
+};
